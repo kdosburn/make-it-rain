0a2684c83b57e10b3bb1ceddad4bdf08e602d098
Matthew Gutierrez <fatchicken007@gmail.com>
2015-09-07 12:42:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Make It Rain
+
+Based on fitztrev's repo, ["Make It Rain"](https://github.com/fitztrev/make-it-rain),
+but built with node and integrated with Celery and Slack.
---
+++ server.js
@@ -0,0 +1,69 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var request    = require('request');
+var yaml       = require('node-yaml-config');
+var q          = require('q');
+var _          = require('underscore');
+
+var app        = express();
+var port       = process.env.PORT || 9000;
+var url        = process.env.URL || 'http://localhost';
+var config     = yaml.load('config.yml');
+
+var getRandomGIF = function() {
+  var deferred = q.defer();
+  var url = 'https://fatchicken007.github.io/make-it-rain/gifs.json';
+
+  request.get(url, {json: true}, function(err,response,body) {
+    var r = _.random(0,body.length-1);
+    deferred.resolve(body[r]);
+  });
+
+  return deferred.promise;
+};
+
+var postToSlack = function(amount, gif) {
+  var deferred = q.defer();
+  var options = {
+    url: config.slack,
+    method: 'POST',
+    body: {
+      username: 'Just got paid',
+      icon_emoji: ':heavy_dollar_sign:',
+      text: amount + ' - ' + gif,
+      unfurl_links: true
+    },
+    json: true
+  };
+
+  request(options, function(err, response, body) {
+    deferred.resolve(body);
+  });
+
+  return deferred.promise;
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/celery', function(req,res,next) {
+  var body = req.body || {};
+
+  if (body.type === 'order.charge.succeeded') {
+    var amount = '$' + body.data.amount;
+
+    getRandomGIF().then(function(gif) {
+      return postToSlack(amount, gif);
+    }).then(function() {
+      res.send('Got paid!');
+    });
+  } else {
+    res.send('Didn\'t get paid :(');
+  }
+});
+
+var server = app.listen(port);
+
+console.log('Listening on ' + url + ':' + port);
